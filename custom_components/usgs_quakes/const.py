ce5169d8e193e4261dfc04f5295f58ba85db22d8
Edison Montes M. <edison.montes@gmail.com>
2025-06-04 19:10:26 -0400

    Update const.py

--- custom_components/usgs_quakes/const.py
+++ custom_components/usgs_quakes/const.py
@@ -8,15 +8,22 @@ CONF_FEED_TYPE = "feed_type"
 
 FEED_TYPES = {
     "past_hour_all": "Past Hour - All magnitudes",
+    "past_hour_1.0": "Past Hour - M1.0+",
+    "past_hour_2.5": "Past Hour - M2.5+",
+    "past_hour_4.5": "Past Hour - M4.5+",
+    "past_hour_significant": "Past Hour - Significant",
     "past_day_all": "Past Day - All magnitudes",
+    "past_day_1.0": "Past Day - M1.0+",
     "past_day_2.5": "Past Day - M2.5+",
     "past_day_4.5": "Past Day - M4.5+",
     "past_day_significant": "Past Day - Significant",
     "past_7days_all": "Past 7 Days - All magnitudes",
+    "past_7days_1.0": "Past 7 Days - M1.0+",
     "past_7days_2.5": "Past 7 Days - M2.5+",
     "past_7days_4.5": "Past 7 Days - M4.5+",
     "past_7days_significant": "Past 7 Days - Significant",
     "past_30days_all": "Past 30 Days - All magnitudes",
+    "past_30days_1.0": "Past 30 Days - M1.0+",
     "past_30days_2.5": "Past 30 Days - M2.5+",
     "past_30days_4.5": "Past 30 Days - M4.5+",
     "past_30days_significant": "Past 30 Days - Significant"
