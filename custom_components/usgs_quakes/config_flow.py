c8a179022920326bd9b69bc60ffed30845ada520
Edison Montes M. <edison.montes@gmail.com>
2025-06-04 18:56:27 -0400

    Update config_flow.py

--- custom_components/usgs_quakes/config_flow.py
+++ custom_components/usgs_quakes/config_flow.py
@@ -1,20 +1,24 @@
-# homeassistant/components/usgs_earthquakes_feed/config_flow.py
-
 from __future__ import annotations
 
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers import selector
 
-from .const import DOMAIN, CONF_RADIUS, CONF_LATITUDE, CONF_LONGITUDE, CONF_MINIMUM_MAGNITUDE
+from .const import (
+    DOMAIN,
+    CONF_RADIUS,
+    CONF_LATITUDE,
+    CONF_LONGITUDE,
+    CONF_MINIMUM_MAGNITUDE,
+    CONF_FEED_TYPE,
+    FEED_TYPES
+)
+
 
 class USGSQuakesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        errors = {}
-
         if user_input is not None:
             return self.async_create_entry(title="USGS Quakes", data=user_input)
 
@@ -25,14 +29,15 @@ class USGSQuakesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Required(CONF_LONGITUDE): float,
                 vol.Required(CONF_RADIUS, default=100.0): float,
                 vol.Optional(CONF_MINIMUM_MAGNITUDE, default=0.0): float,
-            }),
-            errors=errors,
+                vol.Required(CONF_FEED_TYPE, default="past_day_all"): vol.In(FEED_TYPES)
+            })
         )
 
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
         return USGSQuakesOptionsFlowHandler(config_entry)
+
 
 class USGSQuakesOptionsFlowHandler(config_entries.OptionsFlow):
     def __init__(self, config_entry):
@@ -47,5 +52,6 @@ class USGSQuakesOptionsFlowHandler(config_entries.OptionsFlow):
             data_schema=vol.Schema({
                 vol.Required(CONF_RADIUS, default=self.config_entry.data.get(CONF_RADIUS, 100.0)): float,
                 vol.Required(CONF_MINIMUM_MAGNITUDE, default=self.config_entry.data.get(CONF_MINIMUM_MAGNITUDE, 0.0)): float,
-            }),
+                vol.Required(CONF_FEED_TYPE, default=self.config_entry.data.get(CONF_FEED_TYPE, "past_day_all")): vol.In(FEED_TYPES)
+            })
         )
