ebc8fe4dd440f0b2e73b4f2aaf740f2a0998b8eb
Edison Montes M. <edison.montes@gmail.com>
2025-06-04 18:56:03 -0400

    Update const.py

--- custom_components/usgs_quakes/const.py
+++ custom_components/usgs_quakes/const.py
@@ -4,3 +4,20 @@ CONF_RADIUS = "radius"
 CONF_LATITUDE = "latitude"
 CONF_LONGITUDE = "longitude"
 CONF_MINIMUM_MAGNITUDE = "minimum_magnitude"
+CONF_FEED_TYPE = "feed_type"
+
+FEED_TYPES = {
+    "past_hour_all": "Past Hour - All magnitudes",
+    "past_day_all": "Past Day - All magnitudes",
+    "past_day_2.5": "Past Day - M2.5+",
+    "past_day_4.5": "Past Day - M4.5+",
+    "past_day_significant": "Past Day - Significant",
+    "past_7days_all": "Past 7 Days - All magnitudes",
+    "past_7days_2.5": "Past 7 Days - M2.5+",
+    "past_7days_4.5": "Past 7 Days - M4.5+",
+    "past_7days_significant": "Past 7 Days - Significant",
+    "past_30days_all": "Past 30 Days - All magnitudes",
+    "past_30days_2.5": "Past 30 Days - M2.5+",
+    "past_30days_4.5": "Past 30 Days - M4.5+",
+    "past_30days_significant": "Past 30 Days - Significant"
+}
