4a86190ec10e27f46b2280ded979d73b927f8245
Edison Montes M. <edison.montes@gmail.com>
2025-07-09 17:36:40 -0400

    Update config_flow.py

--- custom_components/usgs_quakes/config_flow.py
+++ custom_components/usgs_quakes/config_flow.py
@@ -1,11 +1,19 @@
 from homeassistant import config_entries
 import voluptuous as vol
-
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE, CONF_RADIUS
-from .const import DOMAIN, VALID_FEED_TYPES, DEFAULT_RADIUS_IN_KM, DEFAULT_MINIMUM_MAGNITUDE
+from homeassistant.helpers.selector import selector
 
-CONF_FEED_TYPE = "feed_type"
-CONF_MINIMUM_MAGNITUDE = "minimum_magnitude"
+from .const import (
+    DOMAIN,
+    CONF_FEED_TYPE,
+    CONF_MINIMUM_MAGNITUDE,
+    VALID_FEED_TYPES,
+    DEFAULT_RADIUS_IN_KM,
+    DEFAULT_MINIMUM_MAGNITUDE,
+    FEED_TYPE_FRIENDLY_NAMES,
+    FRIENDLY_NAME_TO_FEED_TYPE,
+)
+
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for USGS Quakes."""
@@ -14,10 +22,28 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="USGS Quakes", data=user_input)
+            # Map friendly name to internal key
+            return self.async_create_entry(
+                title="USGS Quakes",
+                data={
+                    CONF_FEED_TYPE: FRIENDLY_NAME_TO_FEED_TYPE[user_input["feed_type_friendly"]],
+                    CONF_LATITUDE: user_input[CONF_LATITUDE],
+                    CONF_LONGITUDE: user_input[CONF_LONGITUDE],
+                    CONF_RADIUS: user_input[CONF_RADIUS],
+                    CONF_MINIMUM_MAGNITUDE: user_input[CONF_MINIMUM_MAGNITUDE],
+                }
+            )
+
+        # Use friendly names in selector
+        feed_friendly_names = list(FEED_TYPE_FRIENDLY_NAMES.values())
 
         schema = vol.Schema({
-            vol.Required(CONF_FEED_TYPE): vol.In(VALID_FEED_TYPES),
+            vol.Required("feed_type_friendly", default=feed_friendly_names[0]): selector({
+                "select": {
+                    "options": feed_friendly_names,
+                    "mode": "dropdown"
+                }
+            }),
             vol.Required(CONF_LATITUDE, default=self.hass.config.latitude): vol.Coerce(float),
             vol.Required(CONF_LONGITUDE, default=self.hass.config.longitude): vol.Coerce(float),
             vol.Required(CONF_RADIUS, default=DEFAULT_RADIUS_IN_KM): vol.Coerce(float),
