868a01d0e406b4ad45e1157ad4d16dfe33279acb
Edison Montes M. <edison.montes@gmail.com>
2025-07-08 22:27:21 -0400

    Update const.py

--- custom_components/usgs_quakes/const.py
+++ custom_components/usgs_quakes/const.py
@@ -1,30 +1,30 @@
 DOMAIN = "usgs_quakes"
 
-CONF_RADIUS = "radius"
-CONF_LATITUDE = "latitude"
-CONF_LONGITUDE = "longitude"
-CONF_MINIMUM_MAGNITUDE = "minimum_magnitude"
-CONF_FEED_TYPE = "feed_type"
+DEFAULT_MINIMUM_MAGNITUDE = 0.0
+DEFAULT_RADIUS_IN_KM = 50.0
 
-FEED_TYPES = {
-    "past_hour_all": "Past Hour - All magnitudes",
-    "past_hour_1.0": "Past Hour - M1.0+",
-    "past_hour_2.5": "Past Hour - M2.5+",
-    "past_hour_4.5": "Past Hour - M4.5+",
-    "past_hour_significant": "Past Hour - Significant",
-    "past_day_all": "Past Day - All magnitudes",
-    "past_day_1.0": "Past Day - M1.0+",
-    "past_day_2.5": "Past Day - M2.5+",
-    "past_day_4.5": "Past Day - M4.5+",
-    "past_day_significant": "Past Day - Significant",
-    "past_7days_all": "Past 7 Days - All magnitudes",
-    "past_7days_1.0": "Past 7 Days - M1.0+",
-    "past_7days_2.5": "Past 7 Days - M2.5+",
-    "past_7days_4.5": "Past 7 Days - M4.5+",
-    "past_7days_significant": "Past 7 Days - Significant",
-    "past_30days_all": "Past 30 Days - All magnitudes",
-    "past_30days_1.0": "Past 30 Days - M1.0+",
-    "past_30days_2.5": "Past 30 Days - M2.5+",
-    "past_30days_4.5": "Past 30 Days - M4.5+",
-    "past_30days_significant": "Past 30 Days - Significant"
-}
+CONF_FEED_TYPE = "feed_type"
+CONF_MINIMUM_MAGNITUDE = "minimum_magnitude"
+
+VALID_FEED_TYPES = [
+    "past_hour_significant_earthquakes",
+    "past_hour_m45_earthquakes",
+    "past_hour_m25_earthquakes",
+    "past_hour_m10_earthquakes",
+    "past_hour_all_earthquakes",
+    "past_day_significant_earthquakes",
+    "past_day_m45_earthquakes",
+    "past_day_m25_earthquakes",
+    "past_day_m10_earthquakes",
+    "past_day_all_earthquakes",
+    "past_week_significant_earthquakes",
+    "past_week_m45_earthquakes",
+    "past_week_m25_earthquakes",
+    "past_week_m10_earthquakes",
+    "past_week_all_earthquakes",
+    "past_month_significant_earthquakes",
+    "past_month_m45_earthquakes",
+    "past_month_m25_earthquakes",
+    "past_month_m10_earthquakes",
+    "past_month_all_earthquakes",
+]
