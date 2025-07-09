8a47ac5ca6eb7fb966a325de4a6de1922d142af5
Edison Montes M. <edison.montes@gmail.com>
2025-07-08 22:26:06 -0400

    Update __init__.py

--- custom_components/usgs_quakes/__init__.py
+++ custom_components/usgs_quakes/__init__.py
@@ -1,62 +1,19 @@
-from __future__ import annotations
-
-import asyncio
-import logging
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN
-from .sensor import USGSDataUpdateCoordinator
-from aio_geojson_client.usgs_earthquake_feed import USGSEarthquakeFeed
+DOMAIN = "usgs_quakes"
 
-_LOGGER = logging.getLogger(__name__)
-
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up the integration from configuration.yaml (not used)."""
     return True
-
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    latitude = entry.data["latitude"]
-    longitude = entry.data["longitude"]
-    radius = entry.data["radius"]
-    min_mag = entry.data.get("minimum_magnitude", 0.0)
-    feed_type = entry.data.get("feed_type", "past_day_all")
-
-    feed = USGSEarthquakeFeed(
-        home_coordinates=(latitude, longitude),
-        filter_radius=radius,
-        filter_minimum_magnitude=min_mag,
-        feed_type=feed_type
-    )
-
-    coordinator = USGSDataUpdateCoordinator(hass, feed)
-    await coordinator.async_refresh()
-
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = {
-        "coordinator": coordinator,
-    }
-
+    """Set up USGS Quakes from a config entry."""
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["geo_location"])
     )
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "geo_location")
-    )
-
     return True
 
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    unload_ok = all(await asyncio.gather(
-        hass.config_entries.async_forward_entry_unload(entry, "sensor"),
-        hass.config_entries.async_forward_entry_unload(entry, "geo_location")
-    ))
-
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, ["geo_location"])
