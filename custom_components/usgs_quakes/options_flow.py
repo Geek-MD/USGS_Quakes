8753ba26e40893fdecb1d3529c9d410e1babfd73
Edison Montes M. <edison.montes@gmail.com>
2025-07-12 12:23:04 -0400

    Update options_flow.py

--- custom_components/usgs_quakes/options_flow.py
+++ custom_components/usgs_quakes/options_flow.py
@@ -1,57 +1,46 @@
 from homeassistant import config_entries
 import voluptuous as vol
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE, CONF_RADIUS
-from homeassistant.helpers.selector import selector
 
 from .const import (
     DOMAIN,
-    CONF_FEED_TYPE,
-    CONF_MINIMUM_MAGNITUDE,
-    FEED_TYPE_FRIENDLY_NAMES,
+    VALID_FEED_TYPES,
+    FEED_FRIENDLY_NAMES,
     FRIENDLY_NAME_TO_FEED_TYPE,
     DEFAULT_RADIUS_IN_KM,
     DEFAULT_MINIMUM_MAGNITUDE,
 )
 
+CONF_FEED_TYPE = "feed_type"
+CONF_MINIMUM_MAGNITUDE = "minimum_magnitude"
+
+def _get_friendly_feed_selector():
+    """Create selector with friendly names as keys and internal ids as values."""
+    return {FEED_FRIENDLY_NAMES[feed]: feed for feed in VALID_FEED_TYPES}
 
 class UsgsQuakesOptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle a config options flow for USGS Quakes."""
+    """Handle a config flow options."""
 
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+    def __init__(self, config_entry):
         """Initialize options flow."""
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
-        current = {**self.config_entry.data, **self.config_entry.options}
-
         if user_input is not None:
-            return self.async_create_entry(
-                title="",
-                data={
-                    CONF_FEED_TYPE: FRIENDLY_NAME_TO_FEED_TYPE[user_input["feed_type_friendly"]],
-                    CONF_LATITUDE: user_input[CONF_LATITUDE],
-                    CONF_LONGITUDE: user_input[CONF_LONGITUDE],
-                    CONF_RADIUS: user_input[CONF_RADIUS],
-                    CONF_MINIMUM_MAGNITUDE: user_input[CONF_MINIMUM_MAGNITUDE],
-                }
-            )
+            return self.async_create_entry(title="", data=user_input)
 
-        current_feed_type = current.get(CONF_FEED_TYPE)
-        default_friendly = FEED_TYPE_FRIENDLY_NAMES.get(current_feed_type, list(FEED_TYPE_FRIENDLY_NAMES.values())[0])
+        current = self.config_entry.data
 
-        return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema({
-                vol.Required("feed_type_friendly", default=default_friendly): selector({
-                    "select": {
-                        "options": list(FEED_TYPE_FRIENDLY_NAMES.values()),
-                        "mode": "dropdown"
-                    }
-                }),
-                vol.Required(CONF_LATITUDE, default=current.get(CONF_LATITUDE)): vol.Coerce(float),
-                vol.Required(CONF_LONGITUDE, default=current.get(CONF_LONGITUDE)): vol.Coerce(float),
-                vol.Required(CONF_RADIUS, default=current.get(CONF_RADIUS, DEFAULT_RADIUS_IN_KM)): vol.Coerce(float),
-                vol.Required(CONF_MINIMUM_MAGNITUDE, default=current.get(CONF_MINIMUM_MAGNITUDE, DEFAULT_MINIMUM_MAGNITUDE)): vol.Coerce(float),
-            })
-        )
+        # Convert internal feed_type to friendly name for display
+        current_feed = FEED_FRIENDLY_NAMES.get(current.get(CONF_FEED_TYPE), list(FEED_FRIENDLY_NAMES.values())[0])
+
+        schema = vol.Schema({
+            vol.Required(CONF_FEED_TYPE, default=current_feed): vol.In(_get_friendly_feed_selector()),
+            vol.Required(CONF_LATITUDE, default=current.get(CONF_LATITUDE)): vol.Coerce(float),
+            vol.Required(CONF_LONGITUDE, default=current.get(CONF_LONGITUDE)): vol.Coerce(float),
+            vol.Required(CONF_RADIUS, default=current.get(CONF_RADIUS, DEFAULT_RADIUS_IN_KM)): vol.Coerce(float),
+            vol.Required(CONF_MINIMUM_MAGNITUDE, default=current.get(CONF_MINIMUM_MAGNITUDE, DEFAULT_MINIMUM_MAGNITUDE)): vol.Coerce(float),
+        })
+
+        return self.async_show_form(step_id="init", data_schema=schema)
