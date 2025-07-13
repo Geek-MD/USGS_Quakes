a2871ad7349002d220a462e583e3f4c2c6035be5
Edison Montes M. <edison.montes@gmail.com>
2025-07-13 15:01:10 -0400

    Update config_flow.py

--- custom_components/usgs_quakes/config_flow.py
+++ custom_components/usgs_quakes/config_flow.py
@@ -11,3 +11,22 @@ from .const import (
 
 CONF_FEED_TYPE = "feed_type"
 CONF_MINIMUM_MAGNITUDE = "minimum_magnitude"
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for USGS Quakes."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="USGS Quakes", data=user_input)
+
+        schema = vol.Schema({
+            vol.Required(CONF_FEED_TYPE): vol.In(VALID_FEED_TYPES),
+            vol.Required(CONF_LATITUDE, default=self.hass.config.latitude): vol.Coerce(float),
+            vol.Required(CONF_LONGITUDE, default=self.hass.config.longitude): vol.Coerce(float),
+            vol.Required(CONF_RADIUS, default=DEFAULT_RADIUS_IN_KM): vol.Coerce(float),
+            vol.Required(CONF_MINIMUM_MAGNITUDE, default=DEFAULT_MINIMUM_MAGNITUDE): vol.Coerce(float),
+        })
+
+        return self.async_show_form(step_id="user", data_schema=schema)
