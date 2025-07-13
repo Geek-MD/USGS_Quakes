610c763628b2510bc6fb56315a7db3767d24cd78
Edison Montes M. <edison.montes@gmail.com>
2025-07-13 15:01:45 -0400

    Update const.py

--- custom_components/usgs_quakes/const.py
+++ custom_components/usgs_quakes/const.py
@@ -1,10 +1,7 @@
 DOMAIN = "usgs_quakes"
 
-DEFAULT_MINIMUM_MAGNITUDE = 0.0
 DEFAULT_RADIUS_IN_KM = 50.0
-
-CONF_FEED_TYPE = "feed_type"
-CONF_MINIMUM_MAGNITUDE = "minimum_magnitude"
+DEFAULT_MINIMUM_MAGNITUDE = 0.0
 
 VALID_FEED_TYPES = [
     "past_hour_significant_earthquakes",
@@ -26,30 +23,5 @@ VALID_FEED_TYPES = [
     "past_month_m45_earthquakes",
     "past_month_m25_earthquakes",
     "past_month_m10_earthquakes",
-    "past_month_all_earthquakes",
+    "past_month_all_earthquakes"
 ]
-
-FEED_TYPE_FRIENDLY_NAMES = {
-    "past_hour_significant_earthquakes": "Past Hour – Significant",
-    "past_hour_m45_earthquakes": "Past Hour – Magnitude ≥ 4.5",
-    "past_hour_m25_earthquakes": "Past Hour – Magnitude ≥ 2.5",
-    "past_hour_m10_earthquakes": "Past Hour – Magnitude ≥ 1.0",
-    "past_hour_all_earthquakes": "Past Hour – All",
-    "past_day_significant_earthquakes": "Past Day – Significant",
-    "past_day_m45_earthquakes": "Past Day – Magnitude ≥ 4.5",
-    "past_day_m25_earthquakes": "Past Day – Magnitude ≥ 2.5",
-    "past_day_m10_earthquakes": "Past Day – Magnitude ≥ 1.0",
-    "past_day_all_earthquakes": "Past Day – All",
-    "past_week_significant_earthquakes": "Past Week – Significant",
-    "past_week_m45_earthquakes": "Past Week – Magnitude ≥ 4.5",
-    "past_week_m25_earthquakes": "Past Week – Magnitude ≥ 2.5",
-    "past_week_m10_earthquakes": "Past Week – Magnitude ≥ 1.0",
-    "past_week_all_earthquakes": "Past Week – All",
-    "past_month_significant_earthquakes": "Past Month – Significant",
-    "past_month_m45_earthquakes": "Past Month – Magnitude ≥ 4.5",
-    "past_month_m25_earthquakes": "Past Month – Magnitude ≥ 2.5",
-    "past_month_m10_earthquakes": "Past Month – Magnitude ≥ 1.0",
-    "past_month_all_earthquakes": "Past Month – All"
-}
-
-FRIENDLY_NAME_TO_FEED_TYPE = {v: k for k, v in FEED_TYPE_FRIENDLY_NAMES.items()}
