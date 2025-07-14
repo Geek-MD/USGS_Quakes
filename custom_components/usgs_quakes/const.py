e4e6f6975a5e2b8039e5c547117957492dacafbc
Edison Montes M. <edison.montes@gmail.com>
2025-07-14 07:19:33 -0400

    Update const.py

--- custom_components/usgs_quakes/const.py
+++ custom_components/usgs_quakes/const.py
@@ -1,27 +1,9 @@
 DOMAIN = "usgs_quakes"
-
-DEFAULT_RADIUS_IN_KM = 50.0
+DEFAULT_RADIUS = 50.0  # km
 DEFAULT_MINIMUM_MAGNITUDE = 0.0
-
-VALID_FEED_TYPES = [
-    "past_hour_significant_earthquakes",
-    "past_hour_m45_earthquakes",
-    "past_hour_m25_earthquakes",
-    "past_hour_m10_earthquakes",
-    "past_hour_all_earthquakes",
-    "past_day_significant_earthquakes",
-    "past_day_m45_earthquakes",
-    "past_day_m25_earthquakes",
-    "past_day_m10_earthquakes",
-    "past_day_all_earthquakes",
-    "past_week_significant_earthquakes",
-    "past_week_m45_earthquakes",
-    "past_week_m25_earthquakes",
-    "past_week_m10_earthquakes",
-    "past_week_all_earthquakes",
-    "past_month_significant_earthquakes",
-    "past_month_m45_earthquakes",
-    "past_month_m25_earthquakes",
-    "past_month_m10_earthquakes",
-    "past_month_all_earthquakes"
-]
+DEFAULT_SCAN_INTERVAL = 300  # seconds
+CONF_LATITUDE = "latitude"
+CONF_LONGITUDE = "longitude"
+CONF_RADIUS = "radius"
+CONF_MINIMUM_MAGNITUDE = "minimum_magnitude"
+CONF_FEED_TYPE = "feed_type"
