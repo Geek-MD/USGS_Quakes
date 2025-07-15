7c810619293be07d1a835dcbd3fff915d8e878df
Edison Montes M. <edison.montes@gmail.com>
2025-07-15 15:17:20 -0400

    Update config_flow.py

--- custom_components/usgs_quakes/config_flow.py
+++ custom_components/usgs_quakes/config_flow.py
@@ -1,10 +1,8 @@
-from __future__ import annotations
-
 import voluptuous as vol
+
 from homeassistant import config_entries
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
 from homeassistant.core import callback
-from homeassistant.helpers import selector
 
 from .const import (
     DOMAIN,
@@ -13,70 +11,32 @@ from .const import (
     CONF_FEED_TYPE,
     DEFAULT_RADIUS,
     DEFAULT_MINIMUM_MAGNITUDE,
-    DEFAULT_FEED_TYPE,
-    FEED_TYPES,
 )
 
+@callback
+def configured_instances(hass):
+    return hass.config_entries.async_entries(DOMAIN)
 
-class USGSConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for USGS Quakes."""
-
+class USGSQuakesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
+        if configured_instances(self.hass):
+            return self.async_abort(reason="single_instance_allowed")
+
+        errors = {}
+
         if user_input is not None:
             return self.async_create_entry(title="USGS Quakes", data=user_input)
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(CONF_LATITUDE, default=self.hass.config.latitude): float,
-                    vol.Required(CONF_LONGITUDE, default=self.hass.config.longitude): float,
-                    vol.Required(CONF_RADIUS, default=DEFAULT_RADIUS): vol.All(float, vol.Range(min=0)),
-                    vol.Required(CONF_MINIMUM_MAGNITUDE, default=DEFAULT_MINIMUM_MAGNITUDE): vol.All(
-                        float, vol.Range(min=0)
-                    ),
-                    vol.Required(CONF_FEED_TYPE, default=DEFAULT_FEED_TYPE): vol.In(list(FEED_TYPES.keys())),
-                }
-            ),
-        )
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        return USGSOptionsFlowHandler(config_entry)
-
-
-class USGSOptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle options flow for USGS Quakes."""
-
-    def __init__(self, config_entry):
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        """Manage the options."""
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema(
-                {
-                    vol.Required(
-                        CONF_RADIUS,
-                        default=self.config_entry.options.get(CONF_RADIUS, DEFAULT_RADIUS),
-                    ): vol.All(float, vol.Range(min=0)),
-                    vol.Required(
-                        CONF_MINIMUM_MAGNITUDE,
-                        default=self.config_entry.options.get(CONF_MINIMUM_MAGNITUDE, DEFAULT_MINIMUM_MAGNITUDE),
-                    ): vol.All(float, vol.Range(min=0)),
-                    vol.Required(
-                        CONF_FEED_TYPE,
-                        default=self.config_entry.options.get(CONF_FEED_TYPE, DEFAULT_FEED_TYPE),
-                    ): vol.In(list(FEED_TYPES.keys())),
-                }
-            ),
+            data_schema=vol.Schema({
+                vol.Required(CONF_LATITUDE): float,
+                vol.Required(CONF_LONGITUDE): float,
+                vol.Optional(CONF_RADIUS, default=DEFAULT_RADIUS): float,
+                vol.Optional(CONF_MINIMUM_MAGNITUDE, default=DEFAULT_MINIMUM_MAGNITUDE): float,
+                vol.Required(CONF_FEED_TYPE): str,
+            }),
+            errors=errors,
         )
