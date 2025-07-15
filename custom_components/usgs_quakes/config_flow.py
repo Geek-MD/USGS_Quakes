09853a05d20a480ced9b33dadba293b22d3a0a2f
Edison Montes M. <edison.montes@gmail.com>
2025-07-15 15:29:30 -0400

    Update config_flow.py

--- custom_components/usgs_quakes/config_flow.py
+++ custom_components/usgs_quakes/config_flow.py
@@ -1,8 +1,6 @@
-import voluptuous as vol
-
 from homeassistant import config_entries
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
-from homeassistant.core import callback
+import voluptuous as vol
 
 from .const import (
     DOMAIN,
@@ -13,18 +11,13 @@ from .const import (
     DEFAULT_MINIMUM_MAGNITUDE,
 )
 
-@callback
-def configured_instances(hass):
-    return hass.config_entries.async_entries(DOMAIN)
 
 class USGSQuakesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        if configured_instances(self.hass):
+        if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
-
-        errors = {}
 
         if user_input is not None:
             return self.async_create_entry(title="USGS Quakes", data=user_input)
@@ -38,5 +31,4 @@ class USGSQuakesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Optional(CONF_MINIMUM_MAGNITUDE, default=DEFAULT_MINIMUM_MAGNITUDE): float,
                 vol.Required(CONF_FEED_TYPE): str,
             }),
-            errors=errors,
         )
