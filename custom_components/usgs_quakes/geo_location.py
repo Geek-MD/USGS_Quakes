2201f9cc882dfd3ac88227ddb2c9ea1cf8ff61e2
Edison Montes M. <edison.montes@gmail.com>
2025-07-15 15:18:09 -0400

    Update geo_location.py

--- custom_components/usgs_quakes/geo_location.py
+++ custom_components/usgs_quakes/geo_location.py
@@ -1,48 +1,43 @@
-from __future__ import annotations
-
 from datetime import timedelta
 import logging
 
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_registry import async_get
 from homeassistant.helpers.event import async_track_time_interval
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from aio_geojson_client.feed_manager import GeoJsonFeedManager
-from aio_geojson_usgs_earthquakes import USGSEarthquakeFeed
+from aio_geojson_usgs_earthquakes.feed_manager import USGSEarthquakeFeedManager
 
-from .const import (
-    CONF_MINIMUM_MAGNITUDE,
-    CONF_RADIUS,
-    CONF_FEED_TYPE,
-    DEFAULT_SCAN_INTERVAL,
-    DOMAIN,
-)
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
-
 
 async def async_setup_entry(
     hass: HomeAssistant, config_entry: ConfigType, async_add_entities
 ) -> None:
-    """Set up the USGS Quakes geo_location platform from config entry."""
-    config = config_entry.data
-    latitude = config_entry.data["latitude"]
-    longitude = config_entry.data["longitude"]
-    radius = config[CONF_RADIUS]
-    minimum_magnitude = config[CONF_MINIMUM_MAGNITUDE]
-    feed_type = config[CONF_FEED_TYPE]
+    """Set up the USGS Earthquakes geo_location platform."""
+    data = config_entry.data
+    latitude = data["latitude"]
+    longitude = data["longitude"]
+    radius = data["radius"]
+    min_magnitude = data["minimum_magnitude"]
+    feed_type = data["feed_type"]
 
-    websession = async_get_clientsession(hass)
+    session = async_get_clientsession(hass)
 
-    feed = USGSEarthquakeFeed(
-        websession, feed_type, (latitude, longitude), radius, minimum_magnitude
+    manager = USGSEarthquakeFeedManager(
+        hass,
+        async_add_entities,
+        feed_type,
+        (latitude, longitude),
+        radius,
+        min_magnitude,
+        session,
     )
 
-    manager = GeoJsonFeedManager(hass, _LOGGER, feed, async_add_entities)
-
-    hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = manager
+    hass.data.setdefault(DOMAIN, {})[config_entry.entry_id] = {
+        "manager": manager,
+    }
 
     async def update(event_time):
         await manager.update()
