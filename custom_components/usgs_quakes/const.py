f6ee9b7795fd080ea4766ed83e0f23d8a08bd0e5
Edison Montes M. <edison.montes@gmail.com>
2025-07-15 15:53:34 -0400

    Update const.py

--- custom_components/usgs_quakes/const.py
+++ custom_components/usgs_quakes/const.py
@@ -1,10 +1,11 @@
+"""Constants for the USGS Quakes integration."""
+
 DOMAIN = "usgs_quakes"
 
-CONF_LATITUDE = "latitude"
-CONF_LONGITUDE = "longitude"
 CONF_RADIUS = "radius"
 CONF_MINIMUM_MAGNITUDE = "minimum_magnitude"
 CONF_FEED_TYPE = "feed_type"
 
 DEFAULT_RADIUS = 50.0
 DEFAULT_MINIMUM_MAGNITUDE = 0.0
+DEFAULT_FEED_TYPE = "all_week"
