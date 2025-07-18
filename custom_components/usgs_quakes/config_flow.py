0ef464963213712bc0b8737761e6978becc47763
Edison Montes M. <edison.montes@gmail.com>
2025-07-18 19:52:49 -0400

    Update config_flow.py

--- custom_components/usgs_quakes/config_flow.py
+++ custom_components/usgs_quakes/config_flow.py
@@ -6,7 +6,6 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
-from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 
 from .const import (
@@ -18,8 +17,10 @@ from .const import (
     DEFAULT_MINIMUM_MAGNITUDE,
     VALID_FEED_TYPES,
     FEED_TYPE_FRIENDLY_NAMES,
-    FRIENDLY_NAME_TO_FEED_TYPE,
 )
+
+FRIENDLY_NAME_TO_FEED_TYPE = {v: k for k, v in FEED_TYPE_FRIENDLY_NAMES.items()}
+FRIENDLY_NAMES = list(FRIENDLY_NAME_TO_FEED_TYPE.keys())
 
 
 class UsgsQuakesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -28,7 +29,6 @@ class UsgsQuakesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None) -> FlowResult:
-        """Handle the initial step."""
         errors = {}
 
         if user_input is not None:
@@ -37,9 +37,8 @@ class UsgsQuakesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 longitude = float(user_input[CONF_LONGITUDE])
                 radius = float(user_input[CONF_RADIUS])
                 minimum_magnitude = float(user_input[CONF_MINIMUM_MAGNITUDE])
-                feed_type_friendly = user_input[CONF_FEED_TYPE]
-                feed_type = FRIENDLY_NAME_TO_FEED_TYPE.get(feed_type_friendly, feed_type_friendly)
-            except (ValueError, TypeError):
+                feed_type = FRIENDLY_NAME_TO_FEED_TYPE[user_input[CONF_FEED_TYPE]]
+            except (ValueError, TypeError, KeyError):
                 errors["base"] = "invalid_input"
             else:
                 return self.async_create_entry(
@@ -59,10 +58,8 @@ class UsgsQuakesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Optional(CONF_LONGITUDE, default=self.hass.config.longitude): vol.Coerce(float),
                 vol.Required(CONF_RADIUS, default=DEFAULT_RADIUS): vol.Coerce(float),
                 vol.Required(CONF_MINIMUM_MAGNITUDE, default=DEFAULT_MINIMUM_MAGNITUDE): vol.Coerce(float),
-                vol.Required(CONF_FEED_TYPE, default=FEED_TYPE_FRIENDLY_NAMES[VALID_FEED_TYPES[0]]): vol.In(list(FEED_TYPE_FRIENDLY_NAMES.values())),
+                vol.Required(CONF_FEED_TYPE, default=FEED_TYPE_FRIENDLY_NAMES[VALID_FEED_TYPES[0]]): vol.In(FRIENDLY_NAMES),
             }
         )
 
-        return self.async_show_form(
-            step_id="user", data_schema=data_schema, errors=errors
-        )
+        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
