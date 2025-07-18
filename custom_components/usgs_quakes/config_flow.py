4e8931343ca5b46e2da66ee78b93200e20c80acc
Edison Montes M. <edison.montes@gmail.com>
2025-07-18 19:50:40 -0400

    Update config_flow.py

--- custom_components/usgs_quakes/config_flow.py
+++ custom_components/usgs_quakes/config_flow.py
@@ -6,16 +6,19 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_LATITUDE, CONF_LONGITUDE
+from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 
 from .const import (
-    CONF_FEED_TYPE,
-    CONF_MINIMUM_MAGNITUDE,
-    CONF_RADIUS,
-    DEFAULT_MINIMUM_MAGNITUDE,
-    DEFAULT_RADIUS,
     DOMAIN,
+    CONF_RADIUS,
+    CONF_MINIMUM_MAGNITUDE,
+    CONF_FEED_TYPE,
+    DEFAULT_RADIUS,
+    DEFAULT_MINIMUM_MAGNITUDE,
     VALID_FEED_TYPES,
+    FEED_TYPE_FRIENDLY_NAMES,
+    FRIENDLY_NAME_TO_FEED_TYPE,
 )
 
 
@@ -34,7 +37,8 @@ class UsgsQuakesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 longitude = float(user_input[CONF_LONGITUDE])
                 radius = float(user_input[CONF_RADIUS])
                 minimum_magnitude = float(user_input[CONF_MINIMUM_MAGNITUDE])
-                feed_type = user_input[CONF_FEED_TYPE]
+                feed_type_friendly = user_input[CONF_FEED_TYPE]
+                feed_type = FRIENDLY_NAME_TO_FEED_TYPE.get(feed_type_friendly, feed_type_friendly)
             except (ValueError, TypeError):
                 errors["base"] = "invalid_input"
             else:
@@ -55,7 +59,7 @@ class UsgsQuakesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Optional(CONF_LONGITUDE, default=self.hass.config.longitude): vol.Coerce(float),
                 vol.Required(CONF_RADIUS, default=DEFAULT_RADIUS): vol.Coerce(float),
                 vol.Required(CONF_MINIMUM_MAGNITUDE, default=DEFAULT_MINIMUM_MAGNITUDE): vol.Coerce(float),
-                vol.Required(CONF_FEED_TYPE, default=VALID_FEED_TYPES[0]): vol.In(VALID_FEED_TYPES),
+                vol.Required(CONF_FEED_TYPE, default=FEED_TYPE_FRIENDLY_NAMES[VALID_FEED_TYPES[0]]): vol.In(list(FEED_TYPE_FRIENDLY_NAMES.values())),
             }
         )
 
